d8f6165d2cb1db3824655289e04f47348b07a764
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2022-11-02 19:02:57 -0300

    icone adicionado

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
-
 import customtkinter
-
+from PIL import Image,ImageTk
 root = Tk()
 
 root.title()
@@ -13,8 +12,6 @@ root.resizable(width=FALSE, height=FALSE)
 frameTop =  customtkinter.CTkFrame(root,fg_color='white', width=1043,height=40,relief = 'flat',border_width=1,border_color='#DCDCDC')
 frameTop.grid(row=0,column=0,pady=1)
 
-
-
 frameMid =  customtkinter.CTkFrame(root,corner_radius=8,fg_color='#F5F5F5', width=890,height=361,relief = 'raised',border_width=1,border_color='#DCDCDC')
 frameMid.grid(row=1,column=0,pady=1,padx=5,sticky=W)
 
@@ -22,6 +19,11 @@ frameDown =  customtkinter.CTkFrame(root,corner_radius=8,fg_color='#F5F5F5', wid
 frameDown.grid(row=2,column=0,pady=1,padx=5,sticky=W)
 
 
+app_image = Image.open('iconDin.jpg')
+app_image = app_image.resize((35,35))
+app_image = ImageTk.PhotoImage(app_image)
 
+app_logo = Label(frameTop, image=app_image, text="Orçamento Pessoal",width=900,compound=LEFT,padx=5,relief=RAISED,anchor=NW,font=('Roboto 18 bold'),bg='white')
+app_logo.place(x=0,y=0)
 
 root.mainloop()
